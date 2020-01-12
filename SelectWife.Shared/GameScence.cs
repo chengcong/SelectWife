589a005b7f79eb667e459d10928df4a6b19348b1
chengcong <16750571@qq.com>
2020-01-12 10:37:58 +0800

    移植到UWP项目

---
+++ SelectWife.Shared/GameScence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelectWife
+{
+    enum GameScence
+    {
+        MainMenu,//游戏主菜单场景
+        GamePlaying,//游戏场景
+        GameOver,//游戏结束场景
+        About//关于场景
+    }
+}
